e50752f81d621aed9b0bc2e6df2a7d1c20d1a342
Håkon Krogh <hakonhk@gmail.com>
2024-07-26 08:38:37 +0200

    Tests

---
+++ wifi.test.js
@@ -0,0 +1,75 @@
+import { wifiSettings } from "./wifi.js";
+
+test("parses config correctly", () => {
+  expect(
+    wifiSettings.fromConfigContent(`ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=NO
+    
+network={
+  ssid="one"
+  psk="two"
+}`)
+  ).toEqual([
+    {
+      ssid: "one",
+      psk: "two",
+    },
+  ]);
+
+  expect(
+    wifiSettings.fromConfigContent(`ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=NO
+
+network={
+  ssid="one"
+  psk="two"
+}
+  
+network={
+  ssid="three"
+  psk="four"
+}
+  
+`)
+  ).toEqual([
+    {
+      ssid: "one",
+      psk: "two",
+    },
+    {
+      ssid: "three",
+      psk: "four",
+    },
+  ]);
+});
+
+test("sets config correctly", () => {
+  expect(
+    wifiSettings.toConfigContent([
+      {
+        ssid: "one",
+        psk: "two",
+      },
+      {
+        ssid: "three",
+        psk: "four",
+      },
+    ])
+  ).toEqual(`ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=NO
+
+network={
+  ssid="one"
+  psk="two"
+}
+
+network={
+  ssid="three"
+  psk="four"
+}
+
+`);
+});
